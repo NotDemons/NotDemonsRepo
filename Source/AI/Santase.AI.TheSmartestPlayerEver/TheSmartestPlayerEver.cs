e22467921116c8275e968f449a31e7336055a387
HelloWorld15753 <nia_omerska@mail.bg>
2015-11-04 22:50:20 +0200

    added The Smarter Player ever project

---
+++ Source/AI/Santase.AI.TheSmartestPlayerEver/TheSmartestPlayerEver.cs
@@ -0,0 +1,27 @@
+﻿using Santase.Logic.Players;
+using System;
+
+namespace Santase.AI.TheSmartestPlayerEver
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    using Logic.Players;
+    using Logic.Cards;
+
+    public class TheSmartestPlayerEver : BasePlayer
+    {
+        private readonly ICollection<Card> playedCards = new List<Card>();
+
+        private readonly OpponentSuitCardsProvider opponentSuitCardsProvider = new OpponentSuitCardsProvider();
+
+        public override string Name => "Smart Player";
+
+        public override PlayerAction GetTurn(PlayerTurnContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
