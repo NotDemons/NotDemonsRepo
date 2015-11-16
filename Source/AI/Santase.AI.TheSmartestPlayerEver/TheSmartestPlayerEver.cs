39c75f5d1574f087bc1d61c40186ae9be3526823
HelloWorld159753 <nia_omerska@mail.bg>
2015-11-16 22:51:40 +0200

    Started adding scheleton of the code

--- Source/AI/Santase.AI.TheSmartestPlayerEver/TheSmartestPlayerEver.cs
+++ Source/AI/Santase.AI.TheSmartestPlayerEver/TheSmartestPlayerEver.cs
@@ -1,7 +1,4 @@
-﻿using Santase.Logic.Players;
-using System;
-
-namespace Santase.AI.TheSmartestPlayerEver
+﻿namespace Santase.AI.TheSmartestPlayerEver
 {
     using System;
     using System.Collections.Generic;
@@ -17,9 +14,34 @@ namespace Santase.AI.TheSmartestPlayerEver
 
         private readonly OpponentSuitCardsProvider opponentSuitCardsProvider = new OpponentSuitCardsProvider();
 
-        public override string Name => "Smart Player";
+        public override string Name => "The Smartest Player Ever";
 
         public override PlayerAction GetTurn(PlayerTurnContext context)
+        {
+            // When possible change the trump card as this is almost always a good move
+            // Changing trump can be non-optimal when:
+            // 1. Current player is planning to close the game and don't want to give additional points to his opponent
+            // 2. The player will close the game and you will give him additional points by giving him bigger trump card instead of 9
+            // 3. Want to confuse the opponent
+            if (this.PlayerActionValidator.IsValid(PlayerAction.ChangeTrump(), context, this.Cards))
+            {
+                return this.ChangeTrump(context.TrumpCard);
+            }
+
+            if (this.CloseGame(context))
+            {
+                return this.CloseGame();
+            }
+
+            return this.ChooseCard(context);
+        }
+
+        private PlayerAction ChooseCard(PlayerTurnContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        private bool CloseGame(PlayerTurnContext context)
         {
             throw new NotImplementedException();
         }
